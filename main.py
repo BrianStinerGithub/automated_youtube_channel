5f78f2937bead05b75a9f73450b2fc9e04bdfdb8
Brian Stiner <Brian.Stiner@revature.net>
2022-06-01 16:18:10 -0700

    Better comments in main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from scrape_tiktoks import scrape_tiktoks
 from make_compilation import makeCompilation
 from upload_video import upload_video
@@ -16,48 +17,38 @@ num_to_month = {
     10: "Oct", 11: "Nov", 12: "Dec"
 }
 now = datetime.datetime.now()
-videoDirectory = f"./{config.HASHTAG}_" + num_to_month[now.month].upper() + "_" + str(now.year) + "_V" + str(now.day) + "/"
-outputFile = "./" + num_to_month[now.month].upper() + "_" + str(now.year) + "_v" + str(now.day) + ".mp4"
-
-INTRO_VID = config.INTROPATH
+videoDirectory = f"./{config.HASHTAG} {num_to_month[now.month]}_{str(now.day)}_{str(now.year)}"
 OUTRO_VID = config.OUTPUTPATH
-TOTAL_VID_LENGTH = config.VIDEO_LENGTH
-MAX_CLIP_LENGTH = config.MAX_CLIP_LENGTH
-MIN_CLIP_LENGTH = config.MIN_CLIP_LENGTH
-DAILY_SCHEDULED_TIME = "20:00"
 
 def setup():
-    if not os.path.exists(videoDirectory):  os.makedirs(videoDirectory)
-    if not os.path.exists(OUTRO_VID):       os.makedirs(OUTRO_VID)
-    now = datetime.datetime.now()
-    print(f"{now.month}/{now.day}/{now.year} {now.hour}:{now.minute}:{now.second}")
+    if not os.path.exists(videoDirectory): os.makedirs(videoDirectory)  # If path don't exist, make it
+    if not os.path.exists(OUTRO_VID): os.makedirs(OUTRO_VID)            # For the input and output videos 
+    now = datetime.datetime.now()                                       # Get now and show now
+    logging.info(f"Setup run at [{now.hour}:{now.minute}:{now.second} {num_to_month[now.month]}/{now.day}/{now.year}]")
 
 def routine():
-    # Step 0: Setup
-    setup()
-    # Step 1: Scrape Videos
-    scrape_tiktoks()
-    # Step 2: Make Compilation
-    makeCompilation()
-    # Step 3: Upload to Youtube
-    upload_video()
-    # Step 4: Cleanup
-    cleanup()
+    setup()             # Step 0: Setup
+    scrape_tiktoks()    # Step 1: Scrape Videos
+    makeCompilation()   # Step 2: Make Compilation
+    upload_video()      # Step 3: Upload to Youtube 
+    cleanup()           # Step 4: Cleanup
 
 def attemptRoutine():
-    while(1):
-        try:
-            routine()
+    while True:
+        try:                                # When routine runs we stop
+            routine()                       # If it fails, we try again in a minute
             break
-        except OSError as err:
+        except OSError as err:              
             now = datetime.datetime.now()
-            print(f"Routine Failed on {now.hour}:{now.minute}:{now.second} OS error: {0}".format(err))
+            logging.error(f"Routine Failed on {now.hour}:{now.minute}:{now.second} OS error: {0}".format(err))
             time.sleep(60)
 
+# If I use python 3.10, I can use a match case for user input on how often to run the TikTok routine
 
+DAILY_SCHEDULED_TIME = "20:00"
 schedule.every().day.at(DAILY_SCHEDULED_TIME).do(attemptRoutine)
-attemptRoutine()
-while True:
+attemptRoutine()    # Make one video right now
+while True:         # and daily at the chosen time
     schedule.run_pending()  
     time.sleep(60)
 
